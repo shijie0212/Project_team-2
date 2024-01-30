d3d81b24638fa6d44918c118effa2ce74d20b6c4
claudiatanPFB <S10255991@connect.np.edu.sg>
2024-01-30 15:42:06 +0800

    first commit

--- profit_loss.py
+++ profit_loss.py
@@ -17,3 +17,18 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         #and append to the profit and loss list
         profitloss.append([row[0],row[1],row[2],row[3],row[4]])   
 # print(profitloss)
+
+def profitlossFUC():
+    previous = 0
+    deficit_list = []
+    deficit_info=""
+    for item in profitloss:
+        new_difference = int(item[4]) - previous 
+        previous = int(item[4])
+        if new_difference < 0:
+            day=item[0]
+            # deficit_list.append(new_difference)
+            deficit_list.append([abs(new_difference), day])
+            deficit_info += f'[NET PROFIT DEFICIT]Day:{day}, Difference: SGD{abs(new_difference)}\n'
+                # Sort deficit_info by deficit amount in descending order
+    deficit_list.sort(reverse=True)
